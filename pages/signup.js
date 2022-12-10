ccc412fd2feba268819d9615d9189b6c4b2bdb45
Kushal Gohil <2002kushalgohil@gmail.com>
2022-12-10 23:36:15 +0530

    Improved UI

--- pages/signup.js
+++ pages/signup.js
@@ -8,6 +8,7 @@ export default function Signup() {
       <div
         style={{
           height: "100%",
+          width: "100%",
           display: "flex",
           alignItems: "center",
           flexDirection: "column",
