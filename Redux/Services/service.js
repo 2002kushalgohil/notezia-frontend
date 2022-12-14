cd2ef2050278e7b11746400419220c274fae513d
Kushal Gohil <2002kushalgohil@gmail.com>
2022-12-14 20:57:33 +0530

    Added refresh token reload on api

--- Redux/Services/service.js
+++ Redux/Services/service.js
@@ -1,18 +1,47 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const baseQuery = fetchBaseQuery({
+  // baseUrl: "https://notezia-backend.kushalgohil.com/.netlify/functions/api",
+  baseUrl: "http://localhost:9000/.netlify/functions/api",
+  prepareHeaders: (headers) => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      headers.set("Authorization", `Bearer ${accessToken}`);
+    }
+    return headers;
+  },
+});
+
+const baseQueryWithReauth = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result?.error?.status === 403) {
+    const oldRefreshToken = localStorage.getItem("refreshToken");
+    const refreshResult = await baseQuery(
+      `/user/refreshtoken?refreshToken=${oldRefreshToken}`,
+      api,
+      extraOptions
+    );
+
+    if (refreshResult?.data) {
+      const accessToken = refreshResult.data.data.accessToken;
+      const refreshToken = refreshResult.data.data.refreshToken;
+      localStorage.setItem("accessToken", accessToken);
+      localStorage.setItem("refreshToken", refreshToken);
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      localStorage.removeItem("accessToken");
+      localStorage.removeItem("refreshToken");
+      window.location.href = "/login";
+    }
+  }
+
+  return result;
+};
+
 export const noteziaApi = createApi({
   reducerPath: "noteziaApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "https://notezia-backend.kushalgohil.com/.netlify/functions/api",
-    // baseUrl: "http://localhost:9000/.netlify/functions/api",
-    prepareHeaders: (headers, { getState }) => {
-      const accessToken = getState().auth.accessToken;
-      if (accessToken) {
-        headers.set("Authorization", `Bearer ${accessToken}`);
-      }
-      return headers;
-    },
-  }),
+  baseQuery: baseQueryWithReauth,
   tagTypes: ["userProfile"],
   endpoints: (builder) => ({
     signUp: builder.mutation({
